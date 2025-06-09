77741e49399519a313dd5b6fb1cd60fbb68c753b
Sayyeap <156927818+Sayyeap@users.noreply.github.com>
2025-06-09 14:31:53 +0300

    Подлива

---
+++ js/data/artData.js
@@ -0,0 +1,22 @@
+window.artData = [
+    {
+        id: 1,
+        title: "Фото коллеги",
+        path: "assets/story1/images/backgrounds/sexphoto_office.jpg"
+    },
+    {
+        id: 2,
+        title: "Незнакомка из метро",
+        path: "assets/story1/images/backgrounds/sexphoto_metro.jpg"
+    },
+    {
+        id: 3,
+        title: "Игнор соседки",
+        path: "assets/story1/images/backgrounds/roommate_ignore.jpg"
+    },
+    {
+        id: 4,
+        title: "Помощь с коробками",
+        path: "assets/story1/images/backgrounds/roommate_back.jpg"
+    }
+];
